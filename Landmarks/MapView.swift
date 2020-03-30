ce2c63c42d3ae2f5339cdef8aa6228d71fd34b87
yutz <yutz9836@gmail.com>
2020-03-30 20:27:49 +0800

    last

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by cycu on 2020/3/30.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
